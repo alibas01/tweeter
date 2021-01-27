3ead0f4111a794ef8c3d555cc6294c9bc20b52d2
Ali Bas <alibas01@gmail.com>
2021-01-26 19:53:57 -0500

    tweet body completed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('textarea').keyup( function() {
     
     let remaining = 140 - $(this).val().length;
-    console.log($(this).val());
+    // console.log($(this).val());
 
     if (remaining >=0 ) {
     $('output').replaceWith(`<output name="counter" class="counter" for="tweet-text">${remaining}</output>`);
